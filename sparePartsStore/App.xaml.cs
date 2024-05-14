56ab73af5cf1ea3d5e2997dfc8ee853b9242f9d7
Александр Березкин <3.commm@gmail.com>
2024-05-14 09:13:16 +0300

    Add project files.

---
+++ sparePartsStore/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace sparePartsStore
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
