fbea3f900bc91ef3bca7994170a358aa663d7365
Александр Березкин <3.commm@gmail.com>
2024-05-21 11:48:52 +0300

    pageUp

--- sparePartsStore/View/ViewAdministrator/ViewMainPages/PageMainHead.xaml.cs
+++ sparePartsStore/View/ViewAdministrator/ViewMainPages/PageMainHead.xaml.cs
@@ -63,6 +63,8 @@ namespace sparePartsStore.View.ViewAdministrator.ViewMainPages
         // марка
         private void Btn_carBrand(object sender, RoutedEventArgs e)
         {
+            // открываем основное меню
+            basicMenu();
             // очистка фреймов памяти
             ClearMemoryAfterFrame();
             // автоматичесое отображение страницы при входе в учетную запись адинистратора
@@ -72,6 +74,8 @@ namespace sparePartsStore.View.ViewAdministrator.ViewMainPages
         // модель
         private void Btn_carModel(object sender, RoutedEventArgs e)
         {
+            // открываем основное меню
+            basicMenu();
             // очистка фреймов памяти
             ClearMemoryAfterFrame();
             administratorFrame.Navigate(new Uri("/View/ViewAdministrator/ViewWorkingWithData/PageListCarModels.xaml", UriKind.Relative));
@@ -80,6 +84,8 @@ namespace sparePartsStore.View.ViewAdministrator.ViewMainPages
         // агрегат
         private void Btn_Unit(object sender, RoutedEventArgs e)
         {
+            // открываем основное меню
+            basicMenu();
             // очистка фреймов памяти
             ClearMemoryAfterFrame();
             administratorFrame.Navigate(new Uri("/View/ViewAdministrator/ViewWorkingWithData/PageListUnit.xaml", UriKind.Relative));
@@ -88,6 +94,8 @@ namespace sparePartsStore.View.ViewAdministrator.ViewMainPages
         // узел
         private void Btn_Knot(object sender, RoutedEventArgs e)
         {
+            // открываем основное меню
+            basicMenu();
             // очистка фреймов памяти
             ClearMemoryAfterFrame();
             administratorFrame.Navigate(new Uri("/View/ViewAdministrator/ViewWorkingWithData/PageListKnot.xaml", UriKind.Relative));
@@ -96,6 +104,8 @@ namespace sparePartsStore.View.ViewAdministrator.ViewMainPages
         // запчасть
         private void Btn_Details(object sender, RoutedEventArgs e)
         {
+            // открываем основное меню
+            basicMenu();
             // очистка фреймов памяти
             ClearMemoryAfterFrame();
             administratorFrame.Navigate(new Uri("/View/ViewAdministrator/ViewWorkingWithData/PageListAutoparts.xaml", UriKind.Relative));
@@ -104,6 +114,8 @@ namespace sparePartsStore.View.ViewAdministrator.ViewMainPages
         // страна
         private void Btn_Country (object sender, RoutedEventArgs e)
         {
+            // открываем основное меню
+            basicMenu();
             // очистка фреймов памяти
             ClearMemoryAfterFrame();
             administratorFrame.Navigate(new Uri("/View/ViewAdministrator/ViewWorkingWithData/PageListCountry.xaml", UriKind.Relative));
@@ -112,6 +124,8 @@ namespace sparePartsStore.View.ViewAdministrator.ViewMainPages
         // произовдитель
         private void Btn_Manufacture(object sender, RoutedEventArgs e)
         {
+            // открываем основное меню
+            basicMenu();
             // очистка фреймов памяти
             ClearMemoryAfterFrame();
             administratorFrame.Navigate(new Uri("/View/ViewAdministrator/ViewWorkingWithData/PageListManufacture.xaml", UriKind.Relative));
@@ -135,12 +149,74 @@ namespace sparePartsStore.View.ViewAdministrator.ViewMainPages
             GC.WaitForPendingFinalizers();
         }
 
-        // фрейм настройка пользователей
-        private void Btn_UserSetting(object sender, RoutedEventArgs e)
+        // скрываем основное меню и открываем меню настроек
+        private void settingMenu()
         {
+            _basicMenu.Visibility = Visibility.Hidden; // выкл основное меню
+            _settingMenu.Visibility = Visibility.Visible; // вкл меню настроек
+        }
+
+        // скрываем меню настроек и открываем основное меню
+        private void basicMenu()
+        {
+            _settingMenu.Visibility = Visibility.Hidden; // выкл меню настроек
+            _basicMenu.Visibility = Visibility.Visible; // вкл основное меню
+        }
+
+        // пользователи
+        private void Btn_User(object sender, RoutedEventArgs e)
+        {
+            // открываем меню настроек
+            settingMenu();
             // очистка фреймов памяти
             ClearMemoryAfterFrame();
             administratorFrame.Navigate(new Uri("/View/ViewAdministrator/ViewSettingRole/PageListUsers.xaml", UriKind.Relative));
         }
+
+        // роли
+        private void Btn_Role(object sender, RoutedEventArgs e)
+        {
+            // открываем меню настроек
+            settingMenu();
+            // очистка фреймов памяти
+            ClearMemoryAfterFrame();
+            administratorFrame.Navigate(new Uri("/View/ViewAdministrator/ViewSettingRole/PageListRole.xaml", UriKind.Relative));
+        }
+
+        // настройки
+        private void Btn_Setting(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Btn_UserSetting(object  sender, RoutedEventArgs e)
+        {
+            // скрываем кнопку шестерёнка и отображаем кнопку меню
+            _btnSetting.Visibility = Visibility.Hidden;
+            _btnOutMenu.Visibility = Visibility.Visible;
+
+            // открываем меню настроек
+            settingMenu();
+
+            // очистка фреймов памяти
+            ClearMemoryAfterFrame();
+            administratorFrame.Navigate(new Uri("/View/ViewAdministrator/ViewSettingRole/PageListUsers.xaml", UriKind.Relative));
+        }
+
+        // кнопка перехода на основное меню
+        private void Btn_BasicMenu(object sender, RoutedEventArgs e)
+        {
+            // скрываем кнопку меню и отображаем кнопку шестерёнок
+            _btnSetting.Visibility = Visibility.Visible;
+            _btnOutMenu.Visibility= Visibility.Hidden;
+
+            // открываем основеное меню
+            basicMenu();
+
+            // запуск первоначального меню
+            startPage();
+        }
+
+
     }
 }
