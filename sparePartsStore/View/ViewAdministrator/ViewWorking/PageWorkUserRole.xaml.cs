11fc01c0a00ece5a288f600226cdf78e4193158c
Александр Березкин <3.commm@gmail.com>
2024-05-22 18:45:34 +0300

    добавили новый страницы для редактирования данных

---
+++ sparePartsStore/View/ViewAdministrator/ViewWorking/PageWorkUserRole.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace sparePartsStore.View.ViewAdministrator.ViewWorking
+{
+    /// <summary>
+    /// Interaction logic for PageWorkUserRole.xaml
+    /// </summary>
+    public partial class PageWorkUserRole : Page
+    {
+        public PageWorkUserRole()
+        {
+            InitializeComponent();
+        }
+    }
+}
